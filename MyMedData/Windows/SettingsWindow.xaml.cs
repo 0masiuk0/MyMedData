2e9e0f8801b8c3d7246db67647341462f0669d17
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2023-03-05 11:29:20 +0300

    we are ready to start db coding

--- MyMedData/Windows/SettingsWindow.xaml.cs
+++ MyMedData/Windows/SettingsWindow.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MyMedData.Windows;
 
-namespace MyMedData
+namespace MyMedData.Windows
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Логика взаимодействия для SettingsWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class SettingsWindow : Window
 	{
-		public MainWindow()
+		public SettingsWindow()
 		{
 			InitializeComponent();
-		}
-
-		private void AuthorizationButton_Click(object sender, RoutedEventArgs e)
-		{
-			Window usersWindow = new UsersWindow();
-			usersWindow.ShowDialog();
-		}
-
-		private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
-		{
-
 		}
 	}
 }
