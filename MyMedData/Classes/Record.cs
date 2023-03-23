f5a2a9f0d2877a746abca72c6c906ba41c3ad134
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2023-03-23 09:56:32 +0300

    Record classes are amended to implement INotifyPropertyChanged.

--- MyMedData/Classes/Record.cs
+++ MyMedData/Classes/Record.cs
@@ -7,25 +7,126 @@ using LiteDB;
 
 namespace MyMedData
 {
-	public abstract class ExaminationRecord
+	public abstract class ExaminationRecord : INotifyPropertyChanged
 	{
 		[BsonId]
-		public int Id { get; set; }
-		public DateOnly Date { get; set; }	
-		public List<string> Documents { get; set; }
-		
+		private int _id;
+		public int Id
+		{
+			get => _id;
+			set
+			{
+				if (_id != value)
+				{
+					_id = value;
+					OnPropertyChanged(nameof(Id));
+				}
+			}
+		}
+
+		private DateOnly _date;
+		public DateOnly Date
+		{
+			get => _date;
+			set
+			{
+				if (_date != value)
+				{
+					_date = value;
+					OnPropertyChanged(nameof(Date));
+				}
+			}
+		}
+
+		private ObservableCollection<string> _documents = new ObservableCollection<string>();
+		public ObservableCollection<string> Documents
+		{
+			get => _documents;
+			set
+			{
+				if (_documents != value)
+				{
+					if (_documents != null)
+						_documents.CollectionChanged -= Documents_CollectionChanged;
+
+					_documents = value;
+
+					if (_documents != null)
+						_documents.CollectionChanged += Documents_CollectionChanged;
+
+					OnPropertyChanged(nameof(Documents));
+				}
+			}
+		}
+				
+		private Clinic _clinic;
 		[BsonRef(Clinic.DB_COLLECTION_NAME)]
-		public Clinic Clinic { get; set; }
-		public string Comment { get; set; } = "";
+		public Clinic Clinic
+		{
+			get => _clinic;
+			set
+			{
+				if (_clinic != value)
+				{
+					_clinic = value;
+					OnPropertyChanged(nameof(Clinic));
+				}
+			}
+		}
+
+		private string _comment = "";
+		public string Comment
+		{
+			get => _comment;
+			set
+			{
+				if (_comment != value)
+				{
+					_comment = value;
+					OnPropertyChanged(nameof(Comment));
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private void Documents_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			OnPropertyChanged(nameof(Documents));
+		}
 	}
 	
 	public class DoctorExamination : ExaminationRecord
-	{
-		[BsonId]
-		public int DoctorID;
+	{		
+		private int _doctor;
+		[BsonRef(Doctor.DB_COLLECTION_NAME)]
+		public Doctor Doctor
+		{
+			get { return _doctor; }
+			set
+			{
+				_doctor = value;
+				OnPropertyChanged(nameof(Doctor));
+			}
+		}
 
-		[BsonRef(Clinic.DB_COLLECTION_NAME)]
-		public DoctorSpecialty DoctorSpecialty;
+		
+		private DoctorSpecialty _doctorSpecialty;
+		[BsonRef(DoctorSpecialty.DB_COLLECTION_NAME)]
+		public DoctorSpecialty DoctorSpecialty
+		{
+			get { return _doctorSpecialty; }
+			set
+			{
+				_doctorSpecialty = value;
+				OnPropertyChanged(nameof(DoctorSpecialty));
+			}
+		}
 
 		public static string DB_COLLECTION_NAME => "DoctorExaminations";
 	}
