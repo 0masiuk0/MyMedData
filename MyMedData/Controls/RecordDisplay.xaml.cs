879bac95fd30d51a08a35f5b2a6f9e89b910b100
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2023-04-03 19:45:08 +0300

    Some record panel coding

--- MyMedData/Controls/RecordDisplay.xaml.cs
+++ MyMedData/Controls/RecordDisplay.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,34 @@ namespace MyMedData.Controls
 		{
 			InitializeComponent();
 		}
+
+		private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+
+		}
 	}
+
+	public class DateTimeToDateOnlyConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is DateTime dateTime)
+			{
+				return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+			}
+
+			return DependencyProperty.UnsetValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is DateOnly dateOnly)
+			{
+				return new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day);
+			}
+
+			return DependencyProperty.UnsetValue;
+		}
+	}
+
 }
