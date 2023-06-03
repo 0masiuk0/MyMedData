0b6297d9bc3e431fde36f2962f4ea31b87a9bc40
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2023-06-03 12:50:14 +0300

    Document plaque restyling and refactoring

--- MyMedData/Controls/DocumentPlaque.xaml.cs
+++ MyMedData/Controls/DocumentPlaque.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MyMedData.Classes;
+using MyMedData.Windows;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Math;
+
 
 namespace MyMedData.Controls
 {
@@ -24,5 +30,90 @@ namespace MyMedData.Controls
 		{
 			InitializeComponent();
 		}
+
+		bool docIsOpen = false;
+
+		private async Task ViewDocumentAttachment(AttachmentMetaData document)
+		{
+			try
+			{
+				docIsOpen = true;
+				Session session = FindSession();
+				var imageBytes = await document.LoadData(session);
+
+				if (document.DocumentType == DocumentType.JPEG || document.DocumentType == DocumentType.PNG)
+				{
+					BitmapImage image = new BitmapImage();
+					image.BeginInit();
+					image.StreamSource = new MemoryStream(imageBytes);
+					image.EndInit();
+
+					ImageViewWindow imageViewWindow = new ImageViewWindow();
+					imageViewWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;					
+					var width = Min(SystemParameters.WorkArea.Width, 1200);
+					width = Max(SystemParameters.WorkArea.Width / 3, width);
+					var height = Min(SystemParameters.WorkArea.Height, 800);
+					height = Max(SystemParameters.WorkArea.Height / 3, height);
+					imageViewWindow.Height = height;
+					imageViewWindow.Width = width;
+					imageViewWindow.DataContext = image;
+					imageViewWindow.ShowDialog();
+				}
+				else if (document.DocumentType == DocumentType.PDF)
+				{
+					PdfToImageConverter converter = new PdfToImageConverter();
+					await converter.ReadPdfFromBytes(imageBytes);
+
+					PdfViewWindow pdfViewWindow = new PdfViewWindow();
+					pdfViewWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+					var width = Min(SystemParameters.WorkArea.Width, 1200);
+					width = Max(SystemParameters.WorkArea.Width / 3, width);
+					var height = Min(SystemParameters.WorkArea.Height, 800);
+					height = Max(SystemParameters.WorkArea.Height / 3, height);
+					pdfViewWindow.Height = height;
+					pdfViewWindow.Width = width;
+					pdfViewWindow.DataContext = converter.Images.ToList();
+					pdfViewWindow.ShowDialog();
+				}
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show("Непредвиденная ошибка", ex.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			finally
+			{
+				docIsOpen = false;
+			}
+		}
+
+		private async void Image_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton == MouseButtonState.Pressed && !docIsOpen && DataContext is AttachmentMetaData atatchment)
+				await ViewDocumentAttachment(atatchment);
+		}
+
+		private async void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			if (!docIsOpen && DataContext is AttachmentMetaData atatchment)
+				await ViewDocumentAttachment(atatchment);
+		}
+
+		private Session FindSession()
+		{
+			var rightParent = FindParent(this);
+			return ((FrameworkElement)rightParent).DataContext as Session;
+
+			DependencyObject FindParent(DependencyObject child)
+			{
+				DependencyObject parent = VisualTreeHelper.GetParent(child);
+
+				if (parent is FrameworkElement p
+					&& p.DataContext is Session session)
+					return parent;
+				else
+					return FindParent(parent);
+
+			}
+		}		
 	}
 }
