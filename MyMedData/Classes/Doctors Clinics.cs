3a88decf67d36a4d1e69b67e9d0f05ada0b9387a
0masiuk0 <90094421+0masiuk0@users.noreply.github.com>
2023-06-04 20:41:10 +0300

    EntitiesMangerWindow drafted

--- MyMedData/Classes/Doctors Clinics.cs
+++ MyMedData/Classes/Doctors Clinics.cs
@@ -12,7 +12,7 @@ using System.Xml.Linq;
 namespace MyMedData
 {
 #pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
-	public class Doctor : INotifyPropertyChanged, IHasId<string>
+	public class Doctor : INotifyPropertyChanged, IHasId<string>, IHasComment
 	{
 		private string _name;
 		[BsonId]
@@ -71,7 +71,7 @@ namespace MyMedData
 		public string Id => _name; // For IHasId
 	}
 
-	public class Clinic : INotifyPropertyChanged, IHasId<string>
+	public class Clinic : INotifyPropertyChanged, IHasId<string>, IHasComment
 	{
 		private string _name;
 		[BsonId]
@@ -129,7 +129,7 @@ namespace MyMedData
 		public string Id => _name; // For IHasId
 	}
 
-	public class ExaminationType : INotifyPropertyChanged, IHasId<string>
+	public class ExaminationType : INotifyPropertyChanged, IHasId<string>, IHasComment
 	{
 		private string _examinationTypeTitle;
 		[BsonId]
